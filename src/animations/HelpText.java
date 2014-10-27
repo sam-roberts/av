27a1ed4405550f6892da8b2f04c431d45e5cd573
sam-roberts <s4mps0n@gmail.com>
2014-10-27 00:53:07 -0700

    bunch of stuff sorry 4 da lack of deets

---
+++ src/animations/HelpText.java
@@ -0,0 +1,25 @@
+package animations;
+
+import helpers.PublicInformation;
+import processing.core.PApplet;
+
+/**
+ * Created by Sam on 26/10/2014.
+ */
+public class HelpText extends ProcessingAnimation {
+
+
+    public HelpText(PApplet p, PublicInformation info) {
+        super(p, info);
+    }
+
+    @Override
+    protected void drawAnimation() {
+
+    }
+
+    @Override
+    protected void resetTimedAnimation() {
+
+    }
+}
